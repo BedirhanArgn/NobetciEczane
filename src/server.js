d899d2fc4c2595d0b766cfc0e67e831e4b71851b
BedirhanArgn <argunbedir@gmail.com>
2020-08-21 02:41:20 +0300

    updates

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app=express();
 app.use(express.static(__dirname+'/dist/angulareczane'));
 
 
-app.get('*',function(req,res) {
+app.get('/*',function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/angulareczane/index.html'));
 });
 app.listen(process.env.PORT||8080);
