b4ecf8ae2842a80ef1361b1f37ff443e14daf64c
BedirhanArgn <argunbedir@gmail.com>
2020-08-20 22:58:35 +0300

    fixed heroku setting

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const path=require('path');
 const app=express();
-app.use(express.static(__dirname+'/dist/angulareczane'));
+app.use(express.static(__dirname+'../dist/angulareczane'));
 
 app.get('/*',function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/index.html'));
