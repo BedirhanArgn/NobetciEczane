a53d60065645f57f3982a75f99d0938fcfd50596
BedirhanArgn <argunbedir@gmail.com>
2020-08-21 01:18:42 +0300

    deploying heroku

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const express=require('express');
 const path=require('path');
 const app=express();
-app.use(express.static('src/app/dist/angulareczane'));
+app.use(express.static(__dirname+'/dist/angulareczane'));
 
 app.get('/*',function(req,res) {
-    res.sendFile(path.join('src/app/dist/angulareczane/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/angulareczane/index.html'));
 });
 app.listen(process.env.PORT||8080);
 
